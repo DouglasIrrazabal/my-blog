d15174663f92463a3b08453874ae938e7fe717cd
Douglas <douglasirrazabal@gmail.com>
2023-04-08 19:09:38 -0400

    fix blog post path

--- services/getPostContent.ts
+++ services/getPostContent.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import matter from "gray-matter";
 
 const getPostContent = (slug: string) => {
-  const folder = "posts/";
+  const folder = "./posts/";
   const file = `${folder}${slug}.md`;
   const content = fs.readFileSync(file, "utf8");
   const matterResult = matter(content);
