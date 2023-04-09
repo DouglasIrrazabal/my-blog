713eaa6b058f080609c71caa45af6d5b7321909e
Douglas <douglasirrazabal@gmail.com>
2023-04-08 20:04:07 -0400

    refactor to PostPage

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,23 +1,26 @@
 import Markdown from "markdown-to-jsx";
 import getPostsMetadata from "@/services/getPostsMetadata";
 import getPostContent from "@/services/getPostContent";
+import { PostMetadata } from "@/models/PostMetadata";
 
 export const generateStaticParams = async () => {
   const posts = getPostsMetadata();
 
-  return posts.map((post) => {
-    slug: post.slug;
-  });
+  return posts.map(({ slug }: PostMetadata) => slug);
 };
 
 const PostPage = (props: any) => {
-  const slug = props.params.slug;
-  const post = getPostContent(slug);
+  const { slug } = props.params;
+  const {
+    data: { title },
+    content,
+  } = getPostContent(slug);
+
   return (
     <>
-      <h1>{post.data.title}</h1>
+      <h1>{title}</h1>
       <article className="">
-        <Markdown>{post.content}</Markdown>
+        <Markdown>{content}</Markdown>
       </article>
     </>
   );
