fe44f15c81dbaee566dc2177470d9a366438f534
Douglas <douglasirrazabal@gmail.com>
2023-04-09 12:10:28 -0400

    take feedbacks from linkedin and applied

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,36 @@
+import { ArrowIcon } from "@/components/Icons";
+
 const HomePage = () => {
   return (
     <>
-      <p className="mb-2">¡Hola! Soy Doug.</p>
       <p className="mb-2">
-        En la actualidad, me desempeño como{" "}
-        <b>desarrollador de software full-stack</b>.
+        ¡Hola! Soy Doug, <b>desarrollador full-stack y minimalista</b>.
       </p>
       <p className="mb-2">
-        A través de este blog minimalista, comparto mis experiencias y
-        conocimientos sobre cómo{" "}
-        <b>
-          el minimalismo puede mejorar la vida de los desarrolladores de
-          software.
-        </b>{" "}
-        Desde consejos para simplificar el proceso de desarrollo hasta
-        estrategias para manejar el estrés y la ansiedad en el trabajo, aquí
-        encontrarás todo lo que necesitas para ser{" "}
-        <b>más productivo y eficiente.</b>
-      </p>
-      <p className="mb-2">
-        Además, también abordo temas relacionados con el estilo de vida de un
-        desarrollador, como la importancia del{" "}
+        A través de este blog, abordo temas relacionados con el estilo de vida
+        de un desarrollador, como la importancia del{" "}
         <b>equilibrio entre el trabajo y la vida personal,</b> el desarrollo de
         habilidades blandas y la importancia de la formación continua.
       </p>
       <p className="mb-2">
-        En resumen, mi objetivo es ayudar a los desarrolladores de software a
-        ser más eficientes y a tener una vida más equilibrada y satisfactoria.
-        Creo firmemente que el minimalismo es una herramienta poderosa para
-        lograr este objetivo, y espero que mi experiencia y conocimientos puedan
-        ser de utilidad para aquellos que{" "}
+        Mi objetivo es <b>ayudar</b> a los desarrolladores de software a ser más
+        eficientes y a tener una vida más equilibrada y satisfactoria.
+      </p>
+      <p className="mb-2">
+        Creo firmemente que el <b>minimalismo</b> es una herramienta poderosa
+        para lograr este objetivo, y espero que mi experiencia y conocimientos
+        puedan ser de utilidad para aquellos que{" "}
         <b>buscan mejorar su carrera y su vida personal.</b>
       </p>
+      <a
+        className="flex items-center text-neutral-400 hover:text-neutral-100 transition-all"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.linkedin.com/in/douglasirrazabal/"
+      >
+        <ArrowIcon />
+        LinkedIn
+      </a>
     </>
   );
 };
