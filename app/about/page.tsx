903ae278ecb7fc343a5e18dd5c995b2e4954a813
Douglas <douglasirrazabal@gmail.com>
2023-04-13 22:03:49 -0400

    create the about page and move the copy from home page to about page

---
+++ app/about/page.tsx
@@ -0,0 +1,38 @@
+import { ArrowIcon } from "@/components/Icons";
+
+const AboutPage = () => {
+  return (
+    <>
+      <p className="mb-2">
+        ¡Hola! Soy Doug, <b>desarrollador full-stack y minimalista</b>.
+      </p>
+      <p className="mb-2">
+        A través de este blog, abordo temas relacionados con el estilo de vida
+        de un desarrollador, como la importancia del{" "}
+        <b>equilibrio entre el trabajo y la vida personal,</b> el desarrollo de
+        habilidades blandas y la importancia de la formación continua.
+      </p>
+      <p className="mb-2">
+        Mi objetivo es <b>ayudar</b> a los desarrolladores de software a ser más
+        eficientes y a tener una vida más equilibrada y satisfactoria.
+      </p>
+      <p className="mb-2">
+        Creo firmemente que el <b>minimalismo</b> es una herramienta poderosa
+        para lograr este objetivo, y espero que mi experiencia y conocimientos
+        puedan ser de utilidad para aquellos que{" "}
+        <b>buscan mejorar su carrera y su vida personal.</b>
+      </p>
+      <a
+        className="flex items-center text-neutral-400 hover:text-neutral-100 transition-all p-0"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://www.linkedin.com/in/douglasirrazabal/"
+      >
+        <ArrowIcon />
+        LinkedIn
+      </a>
+    </>
+  );
+};
+
+export default AboutPage;
