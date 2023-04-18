03bafac21f28622aa57d5c35dcd99b49bc04b0c3
Douglas <douglasirrazabal@gmail.com>
2023-04-18 12:05:21 -0400

    fix date in content and sort the blogpost

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,15 +4,20 @@ import getPostsMetadata from "@/services/getPostsMetadata";
 
 const ListBlogPosts = () => {
   const postMetadata = getPostsMetadata();
+  const sortedPosts = [...postMetadata].sort(
+    (a, b) => b.publishedAt.getTime() - a.publishedAt.getTime()
+  );
 
-  return postMetadata.map(
+  return sortedPosts.map(
     ({ title, subtitle, publishedAt, slug }: PostMetadata) => (
       <div className="pb-2">
         <Link href={`blog/${slug}`}>
           <h2>{title}</h2>
         </Link>
         <p>{subtitle}</p>
-        <p className="text-sm text-neutral-400">{publishedAt}</p>
+        <p className="text-sm text-neutral-400">
+          {publishedAt.toLocaleDateString("es-CL")}
+        </p>
       </div>
     )
   );
